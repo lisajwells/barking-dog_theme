fd15898ccb5709c8ce62ce18d5d0e550564f0919
Lisa Wells <lisawells999@gmail.com>
2017-01-13 14:11:20 -0500

    position social-contacts-sprite in footer; brighten testimonial bkgd

--- footer.php
+++ footer.php
@@ -18,10 +18,10 @@
         <div class="main-page">
         	<footer id="colophon" class="site-footer inner" role="contentinfo">
         		<div class="contacts">
-                    <a class="contacts-link" href="<?php echo esc_url( __( 'https://www.linkedin.com/in/lisajwells', 'barking-dog' ) ); ?>"></a>
-                    <a class="contacts-link" href="<?php echo esc_url( __( 'https://www.linkedin.com/in/lisajwells', 'barking-dog' ) ); ?>"></a>
-                    <a class="contacts-link" href="<?php echo esc_url( __( 'https://www.linkedin.com/in/lisajwells', 'barking-dog' ) ); ?>"></a>
-        			<a class="contacts-link" href="<?php echo esc_url( __( 'https://www.linkedin.com/in/lisajwells', 'barking-dog' ) ); ?>"></a>
+                    <a class="contacts-link contact-linkedin" href="<?php echo esc_url( __( 'https://www.linkedin.com/in/lisajwells', 'barking-dog' ) ); ?>"></a>
+                    <a class="contacts-link contact-github" href="<?php echo esc_url( __( 'https://github.com/lisajwells', 'barking-dog' ) ); ?>"></a>
+                    <a class="contacts-link contact-generalassembly" href="<?php echo esc_url( __( 'https://profiles.generalassemb.ly/profiles/lisajwells', 'barking-dog' ) ); ?>"></a>
+        			<a class="contacts-link contact-mail" href="<?php echo esc_url( __( 'mailto:lisa@lisajaynewells.com', 'barking-dog' ) ); ?>"></a>
         		</div><!-- .site-info -->
         	</footer><!-- #colophon -->
         </div><!-- .main-page -->
